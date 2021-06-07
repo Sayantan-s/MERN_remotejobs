e1e8524049f819c798c947c2b9289aaddde9cc5a
Sayantan Samanta <sssamanta789@gmail.com>
2021-06-07 14:53:42 +0530

    Added logo

--- src/App.js
+++ src/App.js
@@ -2,106 +2,14 @@ import FormField from "components/elements/FormField.component";
 import { useState } from "react";
 
 function App() {
-  let formArray = [];
-  const [form, setForm] = useState({
-    row1 : [
-      {
-        element : 'input',
-        elementConfig : {
-          type : 'text',
-          placeholder : 'Sayantan',
-          name : 'fullname'
-        },
-        value : '',
-        className: 'w-1/2 mr-2'
-      },
-      {
-        element : 'input',
-        elementConfig : {
-          type : 'email',
-          placeholder : 'sssaanane7....',
-          name : 'email'
-        },
-        value : '',
-        className: 'w-1/2 ml-2'
-      }
-    ],
-    password :   {
-      element : 'input',
-      elementConfig : {
-        type : 'password',
-        placeholder : '6-7 characters',
-        name : 'password'
-      },
-      value : ''
-    },
-    description : {
-      element : 'textarea',
-      elementConfig : {
-        rows : '6',
-        placeholder : 'Add the description...',
-        name : 'description'
-      },
-      value : ''
-    }
-  });
 
-  for(let i in form){
-    if(Array.isArray(form[i])){
-      let newArray = [];
-      Object.values(form[i]).map(data => {
-        return newArray.push({
-          id : data.elementConfig.name,
-          info : data
-        }) 
-      })
-      form[i] = newArray
-    }
-    formArray.push({
-      key : i,
-      data : form[i]
-    })
-  }
-  
-  const handleChange = eve => {
-    const { name, value } = eve.target;
-
-    console.log(name);
-  }
-  
   return (
     <div>
-      <div className="bg-red-400">Heeelooo</div>
-     <form className="w-1/3">
-       {
-         formArray.map(({ key,data }) => {
-           let ele;
-           const isArray = Array.isArray(data);
-            if(isArray){
-                ele = (
-                <>
-                {
-                    data.map(({ id, info }) => (
-                      <FormField 
-                        key={id}
-                        {...info} 
-                        onChange={handleChange}
-                      />
-                    )
-                  )
-                }
-                </>
-              )
-            }
-            else ele = <FormField {...data} onChange={handleChange} />
-            return (
-              <div key={key} className={`flex w-full m-4`}>
-                {ele}
-              </div>
-            )
-         })
-       }
-     </form>
+    <svg xmlns="http://www.w3.org/2000/svg" width="151" height="34" viewBox="0 0 151 34" fill="none">
+        <path d="M15 2C23.2994 2.0869 30 8.76954 30 17C30 25.2305 23.2994 31.9131 15 32L25.7282 17L15 2Z" fill="pink"/>
+        <path d="M14.8411 2.00123C21.5704 2.10506 27 8.78047 27 17C27 25.2195 21.5704 31.8949 14.8412 31.9988C14.7879 31.9994 14.7348 32 14.6816 32C6.57335 32 0 25.2841 0 17C0 8.7159 6.57335 2 14.6816 2C14.7348 2 14.7879 2.00059 14.8411 2.00123ZM22.0372 17C22.0372 12.8495 18.7441 9.48488 14.6816 9.48488C10.6192 9.48488 7.32601 12.8495 7.32601 17C7.32601 21.1505 10.6192 24.5151 14.6816 24.5151C18.744 24.5151 22.0372 21.1505 22.0372 17Z" fill="red"/>
+    </svg>
+       
     </div>
   );
 }
