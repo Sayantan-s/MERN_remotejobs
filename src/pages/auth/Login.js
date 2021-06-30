5355d38a2a59b6016136c9fc9195fde4526805c6
Sayantan Samanta <sssamanta789@gmail.com>
2021-06-30 23:10:58 +0530

    Added sclient form submit stuffs

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -4,12 +4,16 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
 
+    const onSubmitHanlder = eve => {
+        eve.preventDefault()
+    } 
 
     return (
         <div>
-           <form>
+           <form onSubmit={onSubmitHanlder}>
             <input type="email" placeholder="email" value={email} onChange={eve => setEmail(eve.target.value)} />
             <input type="password" placeholder="password" value={password} onChange={eve => setPassword(eve.target.value)} />
+            <button type="submit">Submit</button>
            </form>
         </div>
     )
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -7,15 +7,22 @@ const Register = () => {
     const [confPassword, setConfPassword] = useState('')
     const [phone, setPhone] = useState('')
 
-
+    const onSubmitHanlder = eve => {
+        eve.preventDefault()
+        const data = new FormData(eve.target);
+        for(let i of data.entries()){
+            console.log(i);
+        }
+    } 
     return (
         <div>
-           <form>
+           <form onSubmit={onSubmitHanlder}>
                 <input type="text" placeholder="name" value={name} onChange={eve => setName(eve.target.value)} />
                 <input type="email" placeholder="email" value={email} onChange={eve => setEmail(eve.target.value)} />
                 <input type="numeric" placeholder="phone" value={phone} onChange={eve => setPhone(eve.target.value)} />
                 <input type="password" placeholder="password" value={password} onChange={eve => setPassword(eve.target.value)} />
                 <input type="password" placeholder="confirm password" value={confPassword} onChange={eve => setConfPassword(eve.target.value)} />
+                <button type="submit">Submit</button>
            </form>
         </div>
     )
