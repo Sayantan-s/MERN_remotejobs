800c68288a82ab1a2d0c58fb292118ddbcb86956
Sayantan Samanta <sssamanta789@gmail.com>
2021-08-20 14:20:33 +0530

    Added node for the server

--- server.js
+++ server.js
@@ -48,7 +48,10 @@ app.use(PageNotFoundError);
 app.use(PageError);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 app.listen(port, _=> {
