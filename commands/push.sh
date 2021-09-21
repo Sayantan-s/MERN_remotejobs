879e1d46f6d85ac7ecbf929d385f03d22a81b3e1
Sayantan Samanta <sssamanta789@gmail.com>
2021-09-22 00:28:50 +0530

    Better push message

--- commands/push.sh
+++ commands/push.sh
@@ -1,8 +1,8 @@
 git add .
-tput setaf 3; read -p "-- Enter commit message -- \n" msg
+tput setaf 3; read -p "Enter commit message --> " msg
 tput sgr0;
 git commit -m "$msg"
-tput setaf 3; read -sp "-- Enter branch name -- \n"
+tput setaf 3; read -sp "Enter branch name --> "
 tput sgr0;
 git push -u origin $REPLY
 tput setaf 2; echo "Successfully pushed to $REPLY branch"
